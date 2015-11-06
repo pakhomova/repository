610c3912d2351ab754660de03c29016dcf91a79b
pakhomova <evgpakhomova@gmail.com>
2015-11-06 21:19:42 +0200

    done

--- courses/prog_base/labs/lab2/loops.c
+++ courses/prog_base/labs/lab2/loops.c
@@ -1,13 +1,12 @@
 double calc(int n, int m){
 int i, j;
 double x = 0, calc = 0;
-double pi = 3.1415926;
 for(i = 1; i <= n; i++){
     calc = 0;
     for(j = 1; j <= m; j++){
         calc = calc + pow(j, i);
         }
-    x = x + sin(2*pi/i)*sqrt(calc);
+    x = x + sin(2*M_PI/i)*sqrt(calc);
     }
 return x;
 }
