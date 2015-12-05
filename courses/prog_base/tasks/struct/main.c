c5acf7c5ff91a55f5139889c463245a4f0b2fef3
pakhomova <evgpakhomova@gmail.com>
2015-12-06 00:05:47 +0200

    done

---
+++ courses/prog_base/tasks/struct/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct City{
+char * name, * country;
+double population;
+};
+
+int count(int size, struct City arr[size]){
+int count = 0, i;
+for (i = 1; i < size; i++){
+    if(arr[i].population > 1000000){
+        count++;
+    }
+}
+return count;
+}
+
+void change(struct City * pVar, const char * newValue){
+pVar -> name = newValue;
+}
+
+void print(struct City * pVar){
+    printf("Name: %s\nCountry: %s\nPopulation: %f\n", (pVar -> name), (pVar -> country), (pVar -> population));
+}
+
+int main(void){
+struct City Kitty = {"Kitty", "Ukraine", 1}, London = {"London", "UK", 3000000}, Paris = {"Paris", "France", 2800000};
+struct City arr[3] = {Kitty, London, Paris};
+int i = 0, ind;
+char * newName;
+for (i = 0; i < 3; i++){
+    printf("%i. \n", i+1);
+    print(&arr[i]);
+}
+printf("There are %i cities with population under million.\n", count(3, arr));
+printf("Please, enter the index of city, whose name you want to change:");
+scanf("%i", &ind);
+printf("Please, enter the new name of city: ");
+scanf("%s", newName);
+change(&arr[ind-1], newName);
+printf("New data:\n");
+print(&arr[ind-1]);
+return 0;
+}
