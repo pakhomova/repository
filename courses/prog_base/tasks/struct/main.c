07933830bfe42b369899510b847656e440af4c2e
pakhomova <evgpakhomova@gmail.com>
2015-12-06 00:45:10 +0200

    Done

--- courses/prog_base/tasks/struct/main.c
+++ courses/prog_base/tasks/struct/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct City{
-char * name, * country;
+char name[20], country[20];
 double population;
 };
 
@@ -17,7 +18,7 @@ return count;
 }
 
 void change(struct City * pVar, const char * newValue){
-pVar -> name = newValue;
+strcpy(pVar -> name, newValue);
 }
 
 void print(struct City * pVar){
@@ -28,7 +29,7 @@ int main(void){
 struct City Kitty = {"Kitty", "Ukraine", 1}, London = {"London", "UK", 3000000}, Paris = {"Paris", "France", 2800000};
 struct City arr[3] = {Kitty, London, Paris};
 int i = 0, ind;
-char * newName;
+char newName[20];
 for (i = 0; i < 3; i++){
     printf("%i. \n", i+1);
     print(&arr[i]);
@@ -37,7 +38,8 @@ printf("There are %i cities with population under million.\n", count(3, arr));
 printf("Please, enter the index of city, whose name you want to change:");
 scanf("%i", &ind);
 printf("Please, enter the new name of city: ");
-scanf("%s", newName);
+fflush(stdin);
+fgets(newName, sizeof(newName), stdin);
 change(&arr[ind-1], newName);
 printf("New data:\n");
 print(&arr[ind-1]);
