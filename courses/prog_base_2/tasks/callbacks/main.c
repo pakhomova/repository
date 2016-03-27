e2f130fe00de581234f3418d44b5dfa7d43cea32
pakhomova <evgpakhomova@gmail.com>
2016-03-28 00:30:26 +0300

    done

--- courses/prog_base_2/tasks/callbacks/main.c
+++ courses/prog_base_2/tasks/callbacks/main.c
@@ -15,6 +15,10 @@ void equalFunc (int x) {
     printf ("Equal digit is %i\n", x);
 }
 
+void equalFunc1 (int x) {
+    printf ("Equal!");
+}
+
 int oppositeFunc (int x, int y, int sum) {
     if (x < 0) sum +=x;
         else sum +=y;
@@ -22,8 +26,12 @@ int oppositeFunc (int x, int y, int sum) {
     return sum;
 }
 
+int oppositeFunc1 (int x, int y, int sum) {
+    printf ("Opposite!");
+    return 0;
+}
+
 void randomness (equalCB equal, oppositeCB opposite) {
-    srand(time(NULL));
     int sum = 0;
     while (!kbhit()) {
         Sleep (3);
@@ -31,11 +39,16 @@ void randomness (equalCB equal, oppositeCB opposite) {
         if (x == y) equal (x);
         if (x == -y) sum = opposite (x, y, sum);
     }
+    getch();
 }
 
 int main() {
+    srand(time(NULL));
     equalCB cb1 = equalFunc;
     oppositeCB cb2 = oppositeFunc;
+    equalCB cbAlternative1 = equalFunc1;
+    oppositeCB cbAlternative2 = oppositeFunc1;
     randomness(cb1, cb2);
+    randomness(cbAlternative1, cbAlternative2);
     return 0;
 }
