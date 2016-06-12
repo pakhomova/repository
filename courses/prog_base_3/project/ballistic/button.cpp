9aeb11565dc66eefe0189f3217e77d01af839846
pakhomova <evgpakhomova@gmail.com>
2016-06-12 13:30:10 +0300

    цуалдыв

--- courses/prog_base_3/project/ballistic/button.cpp
+++ courses/prog_base_3/project/ballistic/button.cpp
@@ -1,7 +1,7 @@
 #include "button.h"
 //#include <QPixmap>
 
-Button::Button(int xPos, int yPos, QString simple_Key, QString hover_Key){
+Button::Button(int xPos, int yPos, QString simple_Key, QString hover_Key){ //simple - обычный вид кнопки, hover - вид кнопки, когда на нее наведен курсор
     this->simpleKey = simple_Key;
     this->hoverKey = hover_Key;
     setPixmap(QPixmap(simpleKey));
@@ -14,11 +14,11 @@ void Button::mousePressEvent(QGraphicsSceneMouseEvent *event){
     emit clicked();
 }
 
-void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event){
+void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *event){ //когда на кнопку наводится курсор
     setPixmap(QPixmap(hoverKey));
 }
 
-void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event){
+void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event){ //когда курсор убирается с кнопки
     setPixmap(QPixmap(simpleKey));
 }
 
